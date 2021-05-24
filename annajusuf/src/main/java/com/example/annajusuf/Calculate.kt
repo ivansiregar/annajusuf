79ea8a3cc2bdb031ff873798e7327a942d949c7c
ivan siregar <ivan.siregar@gmail.com>
2021-05-24 09:43:26 +0700

    second commit, bug fix function

--- annajusuf/src/main/java/com/example/annajusuf/Calculate.kt
+++ annajusuf/src/main/java/com/example/annajusuf/Calculate.kt
@@ -5,11 +5,15 @@ class Calculate {
     companion object
     {
         fun tambah (i1: Int, i2: Int) : Int{
-            return (i2 + i2)
+            return (i1 + i2)
         }
 
         fun kali (i1: Int, i2: Int) : Int{
-            return (i2 * i2)
+            return (i1 * i2)
+        }
+
+        fun kurang (i1: Int, i2: Int) : Int{
+            return (i1 - i2)
         }
     }
 
