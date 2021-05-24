6aabb9f3c82dd4ab500310c89b9a36bd60a598a6
ivan siregar <ivan.siregar@gmail.com>
2021-05-24 09:23:40 +0700

    first commit

---
+++ annajusuf/src/main/java/com/example/annajusuf/Calculate.kt
@@ -0,0 +1,17 @@
+package com.example.annajusuf
+
+class Calculate {
+
+    companion object
+    {
+        fun tambah (i1: Int, i2: Int) : Int{
+            return (i2 + i2)
+        }
+
+        fun kali (i1: Int, i2: Int) : Int{
+            return (i2 * i2)
+        }
+    }
+
+
+}
